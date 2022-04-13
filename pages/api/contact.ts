96605b170de1a5bba6a133efbe5464a17870d666
saile515 <elias.jorgensen2006@gmail.com>
2022-04-13 22:15:13 +0200

    Potential fix for contact form 2

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -26,16 +26,22 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
 		from: process.env.EMAIL,
 		to: process.env.EMAIL,
 		subject: `Potentiell kund - ${data.name}`,
-		text: `Email: ${data.email}\nNamn: ${data.name}\nTel: ${data.tel}${data.companyName ? "\nFöretagsnamn: " + data.companyName : ""}\n\nIdé:\n${data.idea}`,
+		text: `Email: ${data.email}\nNamn: ${data.name}${data.tel ? "\nTelefon: " + data.tel : ""}${data.companyName ? "\nFöretagsnamn: " + data.companyName : ""}\n\nIdé:\n${data.idea}`,
 	};
 
 	return new Promise((resolve, reject) => {
 		transporter.sendMail(mailOptions, (error, info) => {
 			if (error) {
-				res.status(400);
+				res.writeHead(302, {
+					Location: "/",
+				});
+				res.end();
 				reject(400);
 			} else {
-				res.redirect("/");
+				res.writeHead(302, {
+					Location: "/",
+				});
+				res.end();
 				resolve(200);
 			}
 		});
