a0161c34f7f4bc561880f68e09fad95adf1d9db4
saile515 <elias.jorgensen2006@gmail.com>
2023-05-06 18:05:12 +0200

    WeOfferEverything content

--- components/WeOfferEverything.tsx
+++ components/WeOfferEverything.tsx
@@ -13,11 +13,11 @@ import {
 	PresentationChartLineIcon,
 } from "@heroicons/react/24/solid";
 
-export default function WeOfferEverything() {
+function Background() {
 	const style = "w-8 h-8 sm:w-12 sm:h-12 text-zinc-700 dark:text-zinc-400 opacity-50 m-auto";
 
 	return (
-		<div className="min-h-screen grid grid-cols-4 grid-flow-column">
+		<div className="min-h-screen w-full grid grid-cols-4 grid-flow-column absolute">
 			<CodeBracketSquareIcon className={style} />
 			<div></div>
 			<PencilIcon className={style} />
@@ -42,6 +42,22 @@ export default function WeOfferEverything() {
 			<PencilSquareIcon className={style} />
 			<div></div>
 			<Cog8ToothIcon className={style} />
+		</div>
+	);
+}
+
+export default function WeOfferEverything() {
+	return (
+		<div className="relative min-h-screen flex flex-col justify-center items-center">
+			<Background />
+			<div className="w-10/12 max-w-lg text-xl xs:text-3xl xs:leading-loose font-bold font-title text-teal-950 dark:text-teal-50 bg-teal-600 dark:bg-teal-600 border-teal-500 dark:border-teal-900 rounded-2xl shadow backdrop-blur-sm bg-opacity-60 dark:bg-opacity-40 px-8 py-2 mb-4 border translate-x-4">
+				<h2>Vi erbjuder allt</h2>
+			</div>
+			<div className="w-10/12 max-w-lg xs:text-xl xs:leading-loose text-teal-950 dark:text-teal-50 bg-teal-400 dark:bg-teal-400 border-teal-100 dark:border-teal-700 rounded-2xl shadow backdrop-blur-sm bg-opacity-50 dark:bg-opacity-40 p-8 border -translate-x-2">
+				Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit odit rerum
+				deleniti reprehenderit pariatur, a velit rem voluptatibus. Amet optio numquam
+				commodi voluptatem, beatae laboriosam a. Neque, officia. Praesentium, reiciendis.
+			</div>
 		</div>
 	);
 }
